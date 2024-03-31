6dee9a6f8b9c5092104eda265f1704b7e273876d
Vladislav Crucerescu <vcrucerescu6@gmail.com>
2024-03-31 15:33:11 +0300

    Proper Parsing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
-use std::{env, process::exit};
+use std::{collections::HashSet, env, process::exit};
 use futures::executor::block_on;
 use linked_hash_set::LinkedHashSet;
+use scraper::{node::Element, ElementRef, Html};
+
+fn query_html<'a>(html: Html, tags: HashSet<&str>) -> Vec<Element> {
+    let mut vector = vec![];
+    query_html_recursive(html.root_element(), &mut vector, &tags);
+    vector
+}
+
+fn query_html_recursive<'a>(element: ElementRef<'a>, vector: &mut Vec<Element>, tags: &HashSet<&str>) {
+    for child_ref in element.child_elements() {
+        let child = child_ref.value();
+        if tags.contains(child.name()) {
+            vector.push(child.clone());
+        }
+        query_html_recursive(child_ref, vector, tags);
+    }
+}
 
 async fn request_get(url: &String) {
     let body = reqwest::get(url)
@@ -8,27 +25,16 @@ async fn request_get(url: &String) {
     .text()
     .await.expect("Failed to parse the response");
 
-    // println!("{}", body);
+    println!("{}", body);
     let html = scraper::Html::parse_document(&body);
-    let selector = scraper::Selector::parse("*").unwrap();
-    let mut html_text_vec: LinkedHashSet<String> = LinkedHashSet::new();
-    for element in html.select(&selector) {
-        
-        // let vector = element.text().collect::<Vec<_>>();
-        // for text in vector.iter() {
-        //     let text = text.trim();
-        //     if !html_text_vec.contains(text) {
-        //         html_text_vec.insert(text.to_string());
-        //     }
-        // }
-    }
-    let mut html_text: String = String::new();
-    for text in html_text_vec.into_iter() {
-        html_text.push_str(text.as_str());
-        html_text.push_str("\n");
+    let tags: HashSet<&str> = HashSet::from_iter(vec!["h1", "h2", "h3", "h4", "h5", "h6", "span", "p", "img", "a", "button"].into_iter());
+    let element_vector = query_html(html, tags);
+    for element in element_vector.iter() {
+        match element.name() {
+            _ => (),
+        }
     }
 
-    println!("{}", html_text);
 }
 
 const HELP_MESSAGE: &'static str = 
